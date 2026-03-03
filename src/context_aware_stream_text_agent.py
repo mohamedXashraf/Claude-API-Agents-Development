322e0c70d1481d776294e280560f2d628fd9d204
mohamedXashraf <mohamed.ashraf.10107@gmail.com>
2026-03-03 09:40:01 +0200

    Chat Assistant - Streaming

---
+++ src/context_aware_stream_text_agent.py
@@ -0,0 +1,45 @@
+from time import sleep
+from anthropic import Anthropic
+from dotenv import load_dotenv
+import os
+
+# Loading ANTHROPIC_API_KEY from .env file
+load_dotenv(override=True)
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+
+# Creating Anthropic SDK Client
+client = Anthropic()
+
+# Define the model to use
+model = "claude-sonnet-4-0"
+
+# Defining the maximum tokens length of answers
+max_tokens = 50
+
+# Messages history list
+messages = []
+
+# Continous chat loop with history context
+while True:
+    message = input("You: ")
+    if (message == "exit"):
+        break
+
+    # Add user input message in to the history list
+    messages.append({"role": "user", "content": message})
+
+    # Request Claude API with Parameters and Message
+    print("Claude: ", end = "")
+    #Creating Stream of replies message
+    with client.messages.stream(
+        model = model,
+        max_tokens = max_tokens,
+        messages = messages
+    ) as stream:
+        for part in stream.text_stream:
+            print(part, end = "")
+            pass
+
+        # Add assistant reply message in to the history list
+        messages.append({"role": "assistant", "content": stream.get_final_message()})
+        print("")
